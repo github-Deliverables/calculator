7ff12641d560a4d223ad4976d59d7ac273392acf
二井　孝文 <niitakafumi@nii-MacBook-Air.local>
2020-01-14 18:19:35 +0900

    ボタン入力

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    @IBOutlet weak var formulaLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // ビューがロードされた時点で式と答えのラベルは空にする
+        formulaLabel.text = ""
+        answerLabel.text = ""
     }
-
-
+    @IBAction func inputFormula(_ sender: UIButton) {
+        // ボタン（Cと=以外）が押されたら式を表示する
+            guard let formulaText = formulaLabel.text else {
+                return
+            }
+            guard let senderedText = sender.titleLabel?.text else {
+                return
+            }
+            formulaLabel.text = formulaText + senderedText
+        }
 }
 
